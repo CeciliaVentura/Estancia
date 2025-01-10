f0e814098cd17d62bdeb77e184557c3295147d71
CeciliaVentura <122043362@upq.edu.mx>
2025-01-10 12:01:17 -0600

    Add files via upload

---
+++ pruebaota.py
@@ -0,0 +1,16 @@
+from ota import OTAUpdater
+from WIFI_CONFIG import SSID, PASSWORD
+
+# URL del repositorio de firmware en GitHub (debe terminar en "/")
+firmware_url = "https://raw.githubusercontent.com/juan-Angel32/OTA/main/"
+
+# Verificación de configuración
+print("SSID:", SSID)
+print("PASSWORD:", PASSWORD)
+print("Firmware URL:", firmware_url)
+
+# Configuración de la actualización OTA
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+
+# Descargar e instalar actualización si está disponible
+ota_updater.download_and_install_update_if_available()
